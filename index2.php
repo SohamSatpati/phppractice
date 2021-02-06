01fe57c396fa8e7f31ee9b69c633be3848146427
SohamSatpati <satpati.soham@gmail.com>
2021-02-06 21:10:44 +0530

    added more code on jquery

--- index2.php
+++ index2.php
@@ -10,12 +10,15 @@ $flag = false;
 
 if(isset($_POST['submit'])){
 
-  if(empty($_POST['fname']) ){
-    $errfname = dispInpErrorMsg("First Name is Required");
-      }
-      else {
-          $fname = validation($_POST['fname']);
-      }
+  $fname  = validation($_POST['fname']);
+  $mname  = validation($_POST['mname']);
+  $lname  = validation($_POST['lname']);
+  $pass = md5($pass);
+  $email  = validation($_POST['email']); 
+  $addr   = validation($_POST["address"]);
+  $mobile = validation($_POST["mobile"]);
+  $dob    = validation($_POST["datepicker"]);
+  $gender = $_POST['MyRadio'];
 
   if(empty($_POST['lang'])){
     echo "";
@@ -26,12 +29,12 @@ if(isset($_POST['submit'])){
     $lang = $_POST["lang"];
   }
 
-    $pass = md5($pass);
-
       $userData = array();
 
       if(!empty($fname) && !empty($lname) && !empty($email) && !empty($pass) &&!empty($repass) && !empty($addr) && !empty($mobile) && !empty($dob) && !empty($lang) && !empty($gender)){
-
+          
+        echo "i am here";
+        
             $flag = true;
             $userData = array(
                   "fname" => $fname,
@@ -98,13 +101,16 @@ if(isset($_POST['submit'])){
           <div class="form-group">
             <label for="textarea">Address</label>
             <textarea class="form-control" id="address" rows="3" name="address" placeholder="Enter your Home address"><?php echo $addr;?></textarea>
+            <span id="addressErrorMsg"></span>
           </div>
 
-          <div class="form-group">
-        <div class="row">    
+    <div class="form-group">
+    <div class="row">    
     <div class="col">
     <label for="exampleFormControlInput1">Mobile</label>
-      <input type="tel" class="form-control" id = "mobile" name = "mobile" placeholder="123-4567-890" pattern="[0-9]{3}-[0-9]{4}-[0-9]{3}" value = "<?php echo $mobile;?>"><small>Format: 123-4567-890</small>
+      <input type="text" class="form-control" id = "mobile" name = "mobile" placeholder="123-4567-890" value = "<?php echo $mobile;?>">
+      <small>Format: 123-4567-890</small><br/>
+      <span id="mobileErrorMsg"></span>
     </div>
 
     <div class="col">
@@ -113,7 +119,7 @@ if(isset($_POST['submit'])){
   <path d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z"/>
   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
 </svg></i>
-    <input data-date-format="dd/mm/yyyy" id="datepicker" class="form-control" name = "datepicker" placeholder="Choose Date of Birth" autocomplete = "off" value = "<?php echo $dob;?>">
+    <input data-date-format="dd/mm/yyyy" id="datepicker" class="form-control" name = "datepicker" placeholder="Choose Date of Birth" autocomplete = "off" value = "<?php echo $dob;?>"><span id="dobErrorMsg"></span>
     
     </div>
         </div>
--- js/myscript.js
+++ js/myscript.js
@@ -9,6 +9,8 @@ $(function (){
   var error_pass  = false;
   var error_repass  = false;
   var error_addr = false;
+  var error_mob = false;
+  var error_dob = false;
 
   var name_regex = /^[a-zA-Z]+$/;
 
@@ -20,8 +22,8 @@ $(function (){
 "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
 
   */
-  var add_regex = /^[0-9a-zA-Z]+$/;
-  var mob_regex = /^[0-9]+$/;
+  
+  var mob_regex = /^[0-9]{3}-[0-9]{4}-[0-9]{3}$/;
 
   
   
@@ -46,6 +48,18 @@ $(function (){
     check_repass();
   });
 
+  $("#address").focusout(function(){
+    check_address();
+  });
+
+  $("#mobile").focusout(function(){
+    check_mobile();
+  });
+
+  $("#datepicker").focusout(function(){
+    check_dob();
+  });
+
   function check_fname(){
     
     var fname_length = $("#fname").val().length;
@@ -57,7 +71,7 @@ $(function (){
           $("#fnameErrorMsg").html("Should be alphabets").css("color", "red");
     }
     else{
-      $("#fnameErrorMsg").html("First name Accepted").css("color", "green");;
+      $("#fnameErrorMsg").html("*First name Accepted").css("color", "green");;
       
     }
     error_fname = true;
@@ -74,7 +88,7 @@ $(function (){
           $("#lnameErrorMsg").html("Should be alphabets").css("color", "red");
     }
     else{
-      $("#lnameErrorMsg").html("Last name Accepted").css("color", "green");;
+      $("#lnameErrorMsg").html("*Last name Accepted").css("color", "green");;
       
     }
     error_lname = true;
@@ -87,7 +101,7 @@ $(function (){
     }
 
     else{
-      $("#emailErrorMsg").html("Email Accepted").css("color", "green");;
+      $("#emailErrorMsg").html("*Email Accepted").css("color", "green");;
       
     }
     error_email = true;
@@ -100,52 +114,105 @@ $(function (){
    if(pass_val.length < 3 ){
     $("#passErrorMsg").html("Password should be 3 characters minimum!").css("color", "red");
       if(pass_val != repass_val){
-        $("#repassErrorMsg").html("Password doesn't match").css("color","red");
+        $("#repassErrorMsg").html("*Password doesn't match").css("color","red");
       }
    }else{
-    $("#passErrorMsg").html("Password accepted!").css("color", "green");
+    $("#passErrorMsg").html("*Password accepted!").css("color", "green");
    }
    
     error_pass = true;
   }
 
   function check_repass(){
-    var pass_val = $("#pass").val();
-    var repass_val= $("#repass").val();
+    var pass_val   = $("#pass").val();
+    var repass_val = $("#repass").val();
 
     if((pass_val == null || pass_val == "") && (repass_val == null || repass_val == "")){
       $("#passErrorMsg").html("Password should be 3 characters minimum!").css("color", "red");
     }
  
-    if(pass_val != repass_val){
-         $("#repassErrorMsg").html("Password doesn't match").css("color","red");
+    else if(pass_val != repass_val){
+         $("#repassErrorMsg").html("*Password doesn't match").css("color","red");
     }
     else{
-      $("#repassErrorMsg").html("Password matched!").css("color", "green");
+      $("#repassErrorMsg").html("*Password matched!").css("color", "green");
     }
     
      error_pass = true;
    }
 
+  function check_address(){
+    var addr = $("#address").val();
+
+    if((addr == "" || addr == null)){
+      $("#addressErrorMsg").html("*Address is required!").css("color", "red");
+    }
+
+    else{
+      $("#addressErrorMsg").html("*Address Accepted").css("color", "green");;
+      
+    }
+    error_addr = true;
+  }
+
+  function check_mobile(){
+    var mob = $("#mobile").val();
+    if(mob == "" || mob == null){
+      $("#mobileErrorMsg").html("Mobile number required").css("color","red");
+    }
+    else if(mob.length < 10 || mob.length > 12){
+      $("#mobileErrorMsg").html("Enter only digits").css("color","red");
+    }
+    else if(!mob.match(mob_regex)){
+      $("#mobileErrorMsg").html("Please choose required format!").css("color","red");
+    }
+    else{
+      $("#mobileErrorMsg").html("*Mobile number accepted").css("color","green");
+    }
+    error_mob = true;
+  }
+
+  function check_dob(){
+    var dob    = $("#datepicker").val();
+    if(dob == "" || dob == null){
+      $("#dobErrorMsg").html("*Dob is required").css("color","red");
+    }
+    else{
+      $("#dobErrorMsg").html("*Dob is accepted").css("color","green");
+    }
+    error_dob = true;
+  }
+
   $(document).on("click","#add_submit",function(){
 
-    var fname = $("#fname").val();
-    var lname = $("#lname").val();
-    //var checkBoxes = $("#Checkbox1").val();
-    var email = $("#email").val();
-    var pass = $("#pass").val();
+    var fname  = $("#fname").val();
+    var lname  = $("#lname").val();
+    var email  = $("#email").val();
+    var pass   = $("#pass").val();
     var repass = $("#repass").val();
+    var addr   = $("#address").val();
+    var mobile = $("#mobile").val();
+    var dob    = $("#datepicker").val();
 
-    if( (lname == "" || lname == null) &&(email == "" || email == null) && (pass == "" || pass == null) && (repass == "" || repass == null)){
+    if((fname == "" || fname == null) && (lname == "" || lname == null) &&(email == "" || email == null) && (pass == "" || pass == null) && (repass == "" || repass == null) && (addr == "" || addr == null) && (mobile == "" || mobile == null) &&(dob == "" || dob == null) ){
 
-     // $("#fnameErrorMsg").html("*First name required!").css("color", "red");
+      $("#fnameErrorMsg").html("*First name required!").css("color", "red");
       $("#lnameErrorMsg").html("*Last name required!").css("color", "red");
       $("#emailErrorMsg").html("*Email required!").css("color", "red");
       $("#passErrorMsg").html("*Password required!").css("color", "red");
       $("#repassErrorMsg").html("*Repassword required!").css("color","red");
+      $("#addressErrorMsg").html("*Address is required!").css("color","red");
+      $("#mobileErrorMsg").html("*Mobile number is required").css("color","red");
+      $("#dobErrorMsg").html("*Dob is required").css("color","red");
       // check_fname();
       // $("#fname").focus();
       return false;
+    }
+    else{
+      console.log(fname);
+      console.log(pass);
+      console.log(mobile);
+      return true;
     }
 
     // else if(lname == "" || lname == null){
